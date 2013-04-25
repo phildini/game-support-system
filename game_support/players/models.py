062927189bd75e10701d384dc4aa14cebecc2d6d
Philip James <philip@eventbrite.com>
2013-04-25 14:45:29 -0700

    Added Player and Battle model, added basic tests.

--- game_support/players/models.py
+++ game_support/players/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from model_utils.models import TimeStampedModel
 
 # Create your models here.
+class Player(TimeStampedModel):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    nickname = models.CharField(max_length=255)
+    wins = models.IntegerField(blank=True, default=0)
+    losses = models.IntegerField(blank=True, default=0)
+    curr_win_streak = models.IntegerField(blank=True, default=0)
+    last_seen = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return self.nickname
+
+
+class Battle(TimeStampedModel):
+    attacker = models.ForeignKey('Player', related_name='attacker')
+    defender = models.ForeignKey('Player', related_name='defender')
+    winner = models.ForeignKey('Player', related_name='winner')
+    start = models.DateTimeField(auto_now_add=True, blank=True)
+    end = models.DateTimeField(auto_now_add=True, blank=True)
+    
+    def __str__(self):
+        return "%s v. %s" % (self.attacker, self.defender,)
--- game_support/players/tests.py
+++ game_support/players/tests.py
@@ -6,6 +6,7 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from models import Player, Battle
 
 
 class SimpleTest(TestCase):
@@ -14,3 +15,18 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+
+class ModelTests(TestCase):
+
+    def test_player_repr(self):
+
+        player = Player(nickname='philip')
+        self.assertEqual(str(player), 'philip')
+
+    def test_battle_repr(self):
+        player1 = Player(nickname='philip')
+        player2 = Player(nickname='chris')
+        battle = Battle(attacker=player1, defender=player2, winner=player2)
+
+        self.assertEqual(str(battle), 'philip v. chris')
